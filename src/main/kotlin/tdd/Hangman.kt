9113d17d1e7058919f9569173267b65bf417042e
ggomez@logyca.com <ggomez@logyca.com>
2020-09-06 18:00:03 -0500

    Se modifica Hangman.kt para que reciba un stream genérico que puede ser un resource

--- src/main/kotlin/tdd/Hangman.kt
+++ src/main/kotlin/tdd/Hangman.kt
@@ -1,6 +1,27 @@
 package tdd
 
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.FileReader
+import java.io.InputStream
+import java.net.URL
+import java.util.stream.Collectors
+import java.util.stream.Stream
+
 class Hangman {
-    fun countAlphabet(word: String, a: Char) =
-        word.count { it == a }
+    private val wordsSet: MutableSet<String>
+
+    constructor(wordsStream: InputStream) {
+        this.wordsSet = wordsStream.bufferedReader().use {
+            it.lines().collect(Collectors.toSet())
+        }
+    }
+
+    fun fetchWord() = fetchWord(5)
+
+    fun fetchWord(requestedLength: Int): String = run {
+        val selectedWord = wordsSet.first { it.length == requestedLength }
+        wordsSet.remove(selectedWord)
+        selectedWord
+    }
 }
--- src/test/kotlin/tdd/TestHangman.kt
+++ src/test/kotlin/tdd/TestHangman.kt
@@ -1,17 +1,42 @@
 package tdd
 
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
+import kotlin.random.Random
+import kotlin.test.assertTrue
 
 class TestHangman {
+
     @Test
-    fun test_alphabetCountInAWord() {
-        val word = "pizza"
-        val alphabet = 'a'
+    fun test_lengthOfFetchedWord() {
+        val hangman = getHangmanFromResource()
+        val word = hangman.fetchWord()
+        assertTrue { word.length == 5 }
+    }
 
-        val hangman= Hangman()
-        val count = hangman.countAlphabet(word, alphabet)
+    @Test
+    fun test_lengthOfFetchedWordRandom() {
+        val random = Random.Default
+        val requestedLength = random.nextInt(5, 10)
+        val hangman = getHangmanFromResource()
+        val word = hangman.fetchWord(requestedLength)
+        assertTrue { word.length == requestedLength }
+    }
 
-        assertEquals(1, count)
+    @Test
+    fun test_uniquenessOfFetchedWord() {
+        val random = Random.Default
+        val usedWords = HashSet<String>()
+        val hangman = getHangmanFromResource()
+
+        for (round in 1..100) {
+            val requestedLength = random.nextInt(5, 10)
+            val word = hangman.fetchWord(requestedLength)
+            assertTrue { usedWords.add(word) }
+        }
+    }
+
+    fun getHangmanFromResource() = run {
+        val wordsStream = TestHangman::class.java.classLoader.getResourceAsStream("WordSource.txt")
+        Hangman(wordsStream)
     }
 }
